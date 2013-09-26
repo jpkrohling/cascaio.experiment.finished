19f0c79fff646030799e57e3b52c9b3463dfb9e2
Juraci Paixão Kröhling <jpkrohling@gmail.com>
2013-09-26 21:02:51 +0200

    Changed the exception mapper to be generic, and added a couple other exceptions to it.

--- backend/src/main/java/com/cascaio/backend/v1/control/GeneralExceptionMapper.java
+++ backend/src/main/java/com/cascaio/backend/v1/control/GeneralExceptionMapper.java
@@ -3,6 +3,7 @@ package com.cascaio.backend.v1.control;
 import org.hibernate.exception.ConstraintViolationException;
 
 import javax.ejb.EJBException;
+import javax.persistence.NoResultException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -13,7 +14,7 @@ import javax.ws.rs.ext.Provider;
  * Time: 5:41 PM
  */
 @Provider
-public class DuplicateRecordExceptionMapper implements ExceptionMapper<EJBException> {
+public class GeneralExceptionMapper implements ExceptionMapper<EJBException> {
 
 	@Override
 	public Response toResponse(EJBException exception) {
@@ -22,7 +23,15 @@ public class DuplicateRecordExceptionMapper implements ExceptionMapper<EJBExcept
 			return Response.status(Response.Status.CONFLICT).build();
 		}
 
-		return Response.status(Response.Status.GONE).build();
+		if (containsCause(exception, NoResultException.class)) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+
+		if (containsCause(exception, IllegalArgumentException.class)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+
+		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 	}
 
 	private boolean containsCause(Exception e, Class<? extends Throwable> causeClazz) {
